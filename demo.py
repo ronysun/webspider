b531004edc45634f47d2053f8d5b333c8a93927e
sunluning <sunluning@t2cloud.net>
2017-01-06 19:07:57 +0800

    add demo.py

---
+++ demo.py
@@ -0,0 +1,24 @@
+#codeing: utf-8
+
+import urllib2
+import re
+import sys
+import chardet
+
+typeEncode = sys.getfilesystemencoding()
+request = urllib2.Request('http://www.dytt8.net/html/gndy/rihan/index.html')
+try:
+    response = urllib2.urlopen(request)
+    cache = response.read()
+    infoencode = chardet.detect(cache).get('encoding','utf-8')
+    print infoencode
+    html = cache.decode('gbk','ignore').encode(typeEncode)
+    #phtml = '<strong>.*</strong>'
+    phtml = '.*index.html">.*</a>.*'
+    menu = re.findall(phtml, html)
+    # menuEncode = chardet.detect(menu).get("encoding","utf-8")
+    # print menuEncode
+    menu = menu.encode('utf-8')
+    print '%s' % menu
+except urllib2.URLError, e:
+    print e.reason
