14393196a5232a95b9a8ce3fc0f671c0bf48c99a
Rony <Rony@192.168.6.6>
2017-01-08 23:06:45 +0800

    modify demo

--- demo.py
+++ demo.py
@@ -1,23 +1,25 @@
 #codeing: utf-8
 
+"""
+
+"""
 import urllib2
 import re
 import sys
 import chardet
 
 typeEncode = sys.getfilesystemencoding()
-request = urllib2.Request('http://www.dytt8.net/html/gndy/rihan/index.html')
+print typeEncode
+request = urllib2.Request('https://movie.douban.com/top250')
 try:
     response = urllib2.urlopen(request)
     cache = response.read()
-    infoencode = chardet.detect(cache).get('encoding','utf-8')
-    print infoencode
-    html = cache.decode('gbk','ignore').encode(typeEncode)
-    #phtml = '<strong>.*</strong>'
-    phtml = '.*index.html">.*</a>.*'
-    menu = re.findall(phtml, html)
-    # menuEncode = chardet.detect(menu).get("encoding","utf-8")
-    menu = menu.encode('utf-8')
-    print '%s' % menu
+    html = cache.decode('utf-8','ignore').encode(typeEncode)
+    regex = r'<span.*?class="title">(.*?)</span>'
+    # menu = re.findall(regex, html)
+    menu = re.findall(regex, html)
+    for movieName in menu:
+        print movieName.decode('utf-8')
+        pass
 except urllib2.URLError, e:
     print e.reason
